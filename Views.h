da12874d7154a5d4d56d991ab35f2bf1bc97a785
Wonkers <e_duckie@hotmail.com>
2020-05-27 12:02:51 +1000

    Update Views.h

--- Views.h
+++ Views.h
@@ -11,8 +11,13 @@ typedef struct ScreenView
 	int Depth;
 	struct View view;
 	struct ViewPort viewPort;
-	struct RasInfo rasInfo[2];	
+	
+	/*2 RasInfos are only required for Dual Playfield*/
+	struct RasInfo rasInfo[2];
+	
+	/*2 Bitmaps are only required for Double Buffering/Dual Playfields*/
 	struct BitMap bitMap[2];
+	
 	struct RastPort rastPort;
 	struct View *oldView;
 	struct CprList *Copper[2][2];
