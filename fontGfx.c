7cc235a06aefeeab8bc7e0156b2dad99edab76ec
Wonkers <e_duckie@hotmail.com>
2021-03-12 15:03:49 +1100

    Update fontGfx.c

--- fontGfx.c
+++ fontGfx.c
@@ -10,15 +10,15 @@ void SetFontGraphicData(struct RastPort *rastport)
     font = rastport->Font;
 
     /*get font characters*/
-	for(j = 0; j < 96/4; j++)
+	for(j = 0; j < 96/sizeof(ULONG); j++)
 	{
 		for(i = 0; i < 8; i++)
 		{
 			value = (ULONG)((font->tf_CharData[ j + (font->tf_Modulo/sizeof(ULONG))*i ]));
-			characters[j*4][i] = (value & 0xff000000)>>24;
-			characters[j*4+1][i] = (value & 0x00ff0000)>>16;
-			characters[j*4+2][i] = (value & 0x0000ff00)>>8;
-			characters[j*4+3][i] = (value & 0x000000ff);
+			characters[j*sizeof(ULONG)][i] = (value & 0xff000000)>>24;
+			characters[j*sizeof(ULONG)+1][i] = (value & 0x00ff0000)>>16;
+			characters[j*sizeof(ULONG)+2][i] = (value & 0x0000ff00)>>8;
+			characters[j*sizeof(ULONG)+3][i] = (value & 0x000000ff);
 		}
 	}
 }
